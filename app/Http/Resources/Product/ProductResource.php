fd9a74a1965a65b9fc156a7a3b276d4113e45992
kerry-evans <findingpartners115@gmail.com>
2019-07-01 16:46:02 +0600

    Product Resource Modify

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,9 +18,14 @@ class ProductResource extends JsonResource
             'name'=>$this->product_name,
             'description'=>$this->details,
             'price'=>$this->price,
-            'stock'=>$this->stock,
+            'totalPrice'=>round( (1- ($this->discount/100))*$this->price, 2),
+            'stock'=>$this->stock == 0? "Out of Stock": $this->stock,
             'discount'=>$this->discount,
             'status'=>($this->status == 1)?'Active':'Inactive',
+            'rating'=> $this->reviews->count() >0 ? round($this->reviews->sum('star')/$this->reviews->count(), 2):"No Rating Yet",
+            'href'=>[
+                'reviews'=>route('reviews.index', $this->id),
+            ]
         ];
     }
 }
