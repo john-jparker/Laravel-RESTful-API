6458a0e86c9c36a0f87faddaeb661ea8db498d77
kerry-evans <findingpartners115@gmail.com>
2019-07-01 14:48:45 +0600

    Product and Review factory and seed done

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,11 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        'product_name'=>$faker->word,
+        'details'=>$faker->paragraph,
+        'price'=>$faker->numberBetween(100, 1000),
+        'stock'=>$faker->randomDigit,
+        'discount'=>$faker->numberBetween(2,30),
+        'status'=>1,
     ];
 });
